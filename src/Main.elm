325cd22e10a1fb1976190a6b34024a614a64bf65
lavirlifiliol <adam.hrbac@gmail.com>
2021-03-11 20:03:33 +0100

    Format file

--- src/Main.elm
+++ src/Main.elm
@@ -5,20 +5,34 @@ import Html exposing (..)
 import Html.Events exposing (..)
 
 
-
 main =
-  Browser.sandbox {init=init, update=update, view=view}
+    Browser.sandbox { init = init, update = update, view = view }
 
-type alias Model = Int
+
+type alias Model =
+    Int
+
 
 init : Model
-init = 0
+init =
+    0
 
-type Msg = Up | Down
+
+type Msg
+    = Up
+    | Down
+
+
 update : Msg -> Model -> Model
-update msg = case msg of
-    Up -> (+) 1
-    Down -> \a -> a - 1
+update msg =
+    case msg of
+        Up ->
+            (+) 1
+
+        Down ->
+            \a -> a - 1
+
 
 view : Model -> Html Msg
-view n = div [] [button [onClick Up] [text "+"], button [onClick Down] [text "-"], h1 [] [String.fromInt n |> text]]
+view n =
+    div [] [ button [ onClick Up ] [ text "+" ], button [ onClick Down ] [ text "-" ], h1 [] [ String.fromInt n |> text ] ]
